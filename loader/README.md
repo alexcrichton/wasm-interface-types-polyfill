60f7b64df56545152ed8f445858c1020f283f134
Alex Crichton <alex@alexcrichton.com>
2019-08-16 10:16:32 -0700

    Add a webpack loader

---
+++ loader/README.md
@@ -0,0 +1,6 @@
+# `wasm-bindings-loader` NPM module
+
+This directory contains a Webpack loader called `wasm-bindings-loader` used for
+executing the `wasm-bindings` NPM module as part of a Webpack build process.
+
+Example usage of this can be found in the Web example.
---
+++ loader/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const wasm_interface_types = require("wasm-interface-types");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+async function run(cx, source) {
+  const name = path.basename(cx.resourcePath, '.wasm');
+  if (name.indexOf('_bg') !== -1)
+    return source;
+  const ret = await wasm_interface_types.compile(name, source, true);
+  // TODO: should figure out how to not need to write to the actual filesystem
+  // here.
+  await writeFileAsync(name + '_bg.wasm', ret.wasm());
+  return ret.js();
+}
+
+module.exports = function (source, a, b) {
+  var callback = this.async();
+
+  run(this, source)
+    .then((x) => {
+      callback(null, x);
+    })
+    .catch((e) => {
+      e.stack = "";
+      callback(e);
+    });
+};
+module.exports.raw = true;
